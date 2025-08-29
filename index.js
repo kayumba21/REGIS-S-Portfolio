8c180d97e5d6e57593fa7ba2ac9f60d0faad4577
kayumba21 <109911976+kayumba21@users.noreply.github.com>
2025-08-29 15:04:29 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const links = document.querySelector('.links');
-
-  menuToggle.addEventListener('click', function() {
-    links.classList.toggle('active');
-  });
- });
